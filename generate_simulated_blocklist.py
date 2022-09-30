1cd630cffe39d58ac44ca8d59d0ab0701b181a43
Wu Tingfeng <wutingfeng@outlook.com>
2022-10-01 04:32:23 +0800

    change to gzip

--- generate_simulated_blocklist.py
+++ generate_simulated_blocklist.py
@@ -1,6 +1,17 @@
-import lzma
+import gzip
+from multiprocessing import Pool
 
-for size in (1000_000, 10_000_000, 100_000_000):
-    with lzma.open(f"blocklist_{size}.txt.xz", "ab") as f:
+
+def create_blocklist(size):
+    message = f"Writing blocklist of size {size:_}..."
+    print(message)
+    with gzip.open(f"blocklist_{size}.txt.gz", "ab") as f:
         for i in range(size):
             f.write(f"{i}.sg\n".encode())
+    print(message, "DONE")
+
+
+if __name__ == "__main__":
+    p = Pool(4)
+    sizes = (1000_000, 10_000_000, 20_000_000, 30_000_000)
+    p.map(create_blocklist, sizes)
